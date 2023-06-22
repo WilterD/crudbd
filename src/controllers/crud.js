894833acc08187a0687861a7a253a62b30de2197
WilterD <wilterd18@gmail.com>
2023-06-21 23:50:33 -0400

    crud

--- src/controllers/crud.js
+++ src/controllers/crud.js
@@ -1,7 +1,6 @@
 import conexion from '../database/db.js';
 
 export const crearRegistroProfesor = async (data) => {
-    console.log(data)
   try {
     const query = 'INSERT INTO profesores (cedulaprof, nombrep, direccionp, telefonop, categoria, dedicacion, fechaing, fechaegr, statusp) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *';
     const values = [
@@ -23,7 +22,6 @@ export const crearRegistroProfesor = async (data) => {
 };
 
 export const actualizarRegistroProfesor = async (data) => {
-  console.log(data)
   try {
     const query = 'UPDATE profesores SET nombrep = $1, direccionp = $2, telefonop = $3, categoria = $4, dedicacion = $5, fechaing = $6, fechaegr = $7, statusp = $8 WHERE cedulaprof = $9 RETURNING *';
     const values = [
@@ -43,3 +41,40 @@ export const actualizarRegistroProfesor = async (data) => {
     throw new Error(error.message);
   }
 };
+
+export const crearRegistroAsignatura = async (data) => {
+  try {
+    const query = 'INSERT INTO asignatura (codasignatura, nombreasig, uc, semestre, taxonomia, statusa) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+    const values = [
+      data.codasignatura,
+      data.nombreasig,
+      data.uc,
+      data.semestre,
+      data.taxonomia,
+      data.statusa
+    ];
+    const result = await conexion.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const actualizarRegistroAsignatura = async (data) => {
+  try {
+    const query = 'UPDATE asignatura SET codasignatura = $1, nombreasig = $2, uc = $3, semestre = $4, taxonomia = $5, statusa = $6 WHERE codasignatura = $1 RETURNING *';
+    const values = [
+      data.codasignatura,
+      data.nombreasig,
+      data.uc,
+      data.semestre,
+      data.taxonomia,
+      data.statusa
+    ];
+    const result = await conexion.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
